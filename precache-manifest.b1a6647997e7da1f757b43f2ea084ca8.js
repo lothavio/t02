de87039e5aefd8d8b3b97b70f58b900c3bdb800a
lothavio <tatopasquetto@gmail.com>
2018-11-06 16:31:42 -0200

    Updates

---
+++ precache-manifest.b1a6647997e7da1f757b43f2ea084ca8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "003733bbb8c05efb11b5103740210fff",
+    "url": "/t02/static/media/foto.003733bb.jpg"
+  },
+  {
+    "revision": "71f394db456a7f0589b1949acce24526",
+    "url": "/t02/static/media/Curriculum.71f394db.pdf"
+  },
+  {
+    "revision": "97b7c9de0faa9655122e",
+    "url": "/t02/static/js/runtime~main.97b7c9de.js"
+  },
+  {
+    "revision": "0dcf580c4c514558a9d8",
+    "url": "/t02/static/js/main.0dcf580c.chunk.js"
+  },
+  {
+    "revision": "7b05a9e3345d60a33041",
+    "url": "/t02/static/js/1.7b05a9e3.chunk.js"
+  },
+  {
+    "revision": "0dcf580c4c514558a9d8",
+    "url": "/t02/static/css/main.36efae07.chunk.css"
+  },
+  {
+    "revision": "ecb911550201150c2dcf030fb57b4c06",
+    "url": "/t02/index.html"
+  }
+];
